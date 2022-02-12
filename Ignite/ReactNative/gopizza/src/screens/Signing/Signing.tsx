ce99ff061867e356aaf8b9c033f281981b209d77
Jhon9191 <joaoantonio15161516@gmail.com>
2022-02-12 12:04:26 -0300

    Finishing from SignIn

--- Ignite/ReactNative/gopizza/src/screens/Signing/Signing.tsx
+++ Ignite/ReactNative/gopizza/src/screens/Signing/Signing.tsx
@@ -1,27 +1,46 @@
-import { View, Text } from 'react-native'
+import { KeyboardAvoidingView, Platform } from 'react-native'
 import React from 'react'
-import { Container } from './styles'
+import { 
+Container, 
+Content, 
+Title, 
+Brand,
+ForgotPassword, 
+ForgotPasswordLabel 
+} from './styles'
 import Input from '@components/Input'
 import Button from '@components/Button'
+import BrandImage from '@assets/brand.png';
 
 const Signing = () => {
   return (
     <Container>
-      <Input 
-      placeholder="E-mail" 
-      type='secondary' 
-      autoCorrect={false}
-      autoCapitalize='none'
-      />
-      <Input 
-      placeholder="Password" 
-      type='secondary' 
-      secureTextEntry
-      />
-      <Button 
-      title="Entrar"
-      type="secondary"
-      />
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <Content>
+          <Brand source={BrandImage}/>
+          <Title>Login</Title>
+          <Input
+            placeholder="E-mail"
+            type='secondary'
+            autoCorrect={false}
+            autoCapitalize='none'
+          />
+          <Input
+            placeholder="Password"
+            type='secondary'
+            secureTextEntry
+          />
+
+          <ForgotPassword>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPassword>
+          
+          <Button
+            title="Entrar"
+            type="secondary"
+          />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   )
 }
