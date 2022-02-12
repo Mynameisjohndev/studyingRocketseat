3950290d25de7d6873fcf2fb7a16c4e76b9c449b
Jhon9191 <joaoantonio15161516@gmail.com>
2022-02-12 08:15:15 -0300

    Creating input component

--- Ignite/ReactNative/gopizza/src/screens/Signing/Signing.tsx
+++ Ignite/ReactNative/gopizza/src/screens/Signing/Signing.tsx
@@ -1,10 +1,22 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Container } from './styles'
+import Input from '@components/Input'
 
 const Signing = () => {
   return (
     <Container>
+      <Input 
+      placeholder="E-mail" 
+      type='secondary' 
+      autoCorrect={false}
+      autoCapitalize='none'
+      />
+      <Input 
+      placeholder="Password" 
+      type='secondary' 
+      secureTextEntry
+      />
     </Container>
   )
 }
