40ee085af11aace510962e0dc075d4555d17c904
Jhon9191 <joaoantonio15161516@gmail.com>
2022-02-22 22:33:24 -0300

    Searching for registered pizzas

--- Ignite/ReactNative/gopizza/src/screens/Home/index.tsx
+++ Ignite/ReactNative/gopizza/src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Alert, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
 import {
     Container,
     Header,
@@ -15,10 +15,36 @@ import { MaterialIcons } from '@expo/vector-icons';
 import { useTheme } from 'styled-components';
 import { TouchableOpacity } from 'react-native';
 import Search from '@components/Search';
-import ProductCard from '@components/ProductCard';
+import ProductCard, { ProductProps } from '@components/ProductCard';
+import firestore from '@react-native-firebase/firestore';
 
 const Home = () => {
     const { COLORS } = useTheme();
+
+    function fatchPizzas(value: string) {
+        const formatedValue = value.toLowerCase().trim();
+        firestore()
+            .collection('pizzas')
+            .orderBy('name_insensitive')
+            .startAt(formatedValue)
+            .endAt(`${formatedValue}\uf8ff`)
+            .get()
+            .then(response => {
+                const data = response.docs.map(doc => {
+                    return {
+                        id: doc.id,
+                        ...doc.data()
+                    }
+                }) as ProductProps[];
+                console.log(data)
+            })
+            .catch(()=> Alert.alert('Consulta', 'Não foi possivel realizar a consulta'));
+    }
+
+    useEffect(()=>{
+        fatchPizzas('');
+    },[])
+
     return (
         <Container>
             <Header>
@@ -35,7 +61,7 @@ const Home = () => {
                 <MenuTitle>Cardápio</MenuTitle>
                 <MenuItensNumber>10 pizzas</MenuItensNumber>
             </MenuHeader>
-            <ProductCard data={{id: '1', name: 'Pizza', description:  'Teste de descrição', photo_url: 'https://2.bp.blogspot.com/-wNgz-XOKW_o/Um-krPebaNI/AAAAAAAAKbI/NM3fmL-j6Vo/s1600/pizza_de_chourico_com_tomate_cereja_e_queijo_President_vista_de_cima.jpg'}}/>
+            <ProductCard data={{ id: '1', name: 'Pizza', description: 'Teste de descrição', photo_url: 'https://2.bp.blogspot.com/-wNgz-XOKW_o/Um-krPebaNI/AAAAAAAAKbI/NM3fmL-j6Vo/s1600/pizza_de_chourico_com_tomate_cereja_e_queijo_President_vista_de_cima.jpg' }} />
         </Container>
     )
 }
