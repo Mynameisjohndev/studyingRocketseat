e19f8e05ea7ba44d3251910d9d9fdd747603a5bb
Jhon9191 <joaoantonio15161516@gmail.com>
2022-03-12 11:22:57 -0300

    Order Component Style

---
+++ Ignite/ReactNative/gopizza/src/components/OrderCard/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {
+Container,
+} from "./styles";
+const OrderCard = () => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default OrderCard
---
+++ Ignite/ReactNative/gopizza/src/components/OrderCard/styles.ts
@@ -0,0 +1,74 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import styled, { css } from 'styled-components/native';
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
+type ContainerProps = {
+    index: number;
+}
+
+export type StatusTypeProps = 'Preparando' | 'Pronto' | 'Entregue'
+
+type StatusProps = {
+    status: StatusTypeProps;
+}
+
+export const Container = styled.View<ContainerProps>`
+    width:50%;
+    align-items: center;
+    padding: 24px;
+    ${({ theme, index }) => css`
+        border-right-width: ${index %2 > 0 ? 0 : 1 };
+        border-right-color: ${theme.COLORS.SHAPE };
+    `}
+`;
+
+export const Image = styled.Image`
+    width: 140px;
+    height: 140px;
+    border-radius: 52px;
+`;
+
+export const Name = styled.Text`
+   font-size: 20px;
+   margin-top: 21px;
+   ${({theme})=>css`
+   font-family: ${theme.FONTS.TITLE};
+   color: ${theme.COLORS.SECONDARY_900};
+   `}
+`;
+
+export const Description = styled.Text`
+   font-size: 14px;
+   margin-top: 11px;
+   ${({theme})=>css`
+   font-family: ${theme.FONTS.TITLE};
+   color: ${theme.COLORS.SECONDARY_900};
+   `}
+`;
+
+export const StatusContainer = styled.View<StatusProps>`
+    padding: 4px 16px;
+    border-radius: 12px;
+    margin-top: 16px;
+    align-items: center;
+    justify-content: center;
+    ${({theme, status})=> status === 'Preparando' && css`
+        background-color: ${theme.COLORS.ALERT_50};
+        border: 1px solid ${theme.COLORS.ALERT_900};
+    `};
+    ${({theme, status})=> status === 'Pronto' && css`
+        background-color: ${theme.COLORS.SUCCESS_900};
+    `};
+    ${({theme, status})=> status === 'Entregue' && css`
+        background-color: ${theme.COLORS.SECONDARY_900};
+    `};
+`;
+
+export const StatusLabel = styled.Text<StatusProps>`
+    font-size: 12px;
+    line-height: 20px;
+    ${({theme, status}) => css`
+        font-family: ${theme.FONTS.TEXT};
+        color: ${status === 'Preparando' ? theme.COLORS.ALERT_900 : theme.COLORS.TITLE}
+    `};
+`
