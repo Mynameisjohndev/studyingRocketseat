5fa6262fbea5c10381bcd4c4b0acfc5267b354cc
Jhon9191 <joaoantonio15161516@gmail.com>
2022-03-11 22:59:18 -0300

    Order form

--- Ignite/ReactNative/gopizza/src/screens/Order/index.tsx
+++ Ignite/ReactNative/gopizza/src/screens/Order/index.tsx
@@ -1,28 +1,61 @@
 import React, { useState } from "react";
 import { Platform } from "react-native";
-import { Container, Header, Photo, Sizes } from "./styles";
+import {
+  Container,
+  Header,
+  Photo,
+  Sizes,
+  Form,
+  Title,
+  FormRow,
+  InputGroup,
+  Label,
+  Price,
+  ContentScroll,
+} from "./styles";
 import ButtonBack from "@components/ButtonBack";
 import RadioButton from "@components/RadioButton";
+import Button from "@components/Button";
+import Input from "@components/Input";
 import { PIZZA_TYPES } from "@utils/pizzaTypes";
 
 const Order = () => {
   const [selectedItem, setSelectedItem] = useState("");
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
-      <Header>
-        <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
-      </Header>
-      <Photo source={{ uri: "http://github.com/Myanameisjohndev.png" }} />
-      <Sizes>
-        {PIZZA_TYPES.map((pizza) => (
-          <RadioButton 
-          key={pizza.id} 
-          onPress={() => setSelectedItem(pizza.id)}
-          selected={selectedItem === pizza.id} 
-          title={pizza.name} 
-          />
-        ))}
-      </Sizes>
+      <ContentScroll>
+        <Header>
+          <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
+        </Header>
+        <Photo source={{ uri: "http://github.com/Myanameisjohndev.png" }} />
+        <Form>
+          <Title>Nome da pizza</Title>
+          <Label>Selecione um tamanho</Label>
+          <Sizes>
+            {PIZZA_TYPES.map((pizza) => (
+              <RadioButton
+                key={pizza.id}
+                onPress={() => setSelectedItem(pizza.id)}
+                selected={selectedItem === pizza.id}
+                title={pizza.name}
+              />
+            ))}
+          </Sizes>
+
+          <FormRow>
+            <InputGroup>
+              <Label>Número da mesa</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+            <InputGroup>
+              <Label>Quantidade</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+          </FormRow>
+          <Price>Valor de R$ 90,90</Price>
+          <Button title="Confirmar pedido" />
+        </Form>
+      </ContentScroll>
     </Container>
   );
 };
--- Ignite/ReactNative/gopizza/src/screens/Order/styles.ts
+++ Ignite/ReactNative/gopizza/src/screens/Order/styles.ts
@@ -1,17 +1,16 @@
-import theme from '@src/theme';
 import { LinearGradient } from 'expo-linear-gradient';
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const Container = styled.KeyboardAvoidingView`
     flex:1;
-`
+`;
 
 export const Header = styled(LinearGradient).attrs(({ theme })=>({
     colors: theme.COLORS.GRADIENT,
 }))`
     padding: ${getStatusBarHeight() + 34}px 24px 0px;
-`
+`;
 
 export const Photo = styled.Image`
     width: 250px;
@@ -20,11 +19,62 @@ export const Photo = styled.Image`
     align-self: center;
     position: relative;
     top: -120px;
-`
+`;
 
 export const Sizes = styled.View`
     width: 100%;
     flex-direction: row;
     justify-content: space-between;
     margin-bottom: 40px;
-`
+`;
+
+export const Form = styled.View`
+    width: 100%;
+    margin-top: -120px;
+    padding: 24px;
+`;
+
+export const Title = styled.Text`
+    font-size: 32px;
+    margin-bottom: 32px;
+    text-align: center;
+    ${({ theme }) => css`
+        font-family: ${theme.FONTS.TITLE};
+        color: ${theme.COLORS.SECONDARY_900}
+    `}
+`;
+
+export const Label = styled.Text`
+    font-size: 14px;
+    margin-bottom: 16px;
+    ${({ theme }) => css`
+        font-family: ${theme.FONTS.TEXT};
+        color: ${theme.COLORS.SECONDARY_900}
+    `}
+`;
+
+export const FormRow = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const InputGroup = styled.View`
+    width: 48%;
+`;
+
+export const Price = styled.Text`
+    font-size: 14px;
+    margin: 24px 0px;
+    align-self: flex-end;
+    ${({ theme }) => css`
+        font-family: ${theme.FONTS.TEXT};
+        color: ${theme.COLORS.SECONDARY_900}
+    `}
+`;
+
+export const ContentScroll = styled.ScrollView.attrs({
+    showsVerticalScrollIndicator : false,
+})`
+    background-color: ${({theme})=>theme.COLORS.BACKGROUND};
+`;
