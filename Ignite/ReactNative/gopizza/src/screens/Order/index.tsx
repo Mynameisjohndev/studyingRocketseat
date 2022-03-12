5ce02e590e84f270c37b5f824c99d842846fbde9
Jhon9191 <joaoantonio15161516@gmail.com>
2022-03-11 21:15:56 -0300

    Order header

--- Ignite/ReactNative/gopizza/src/screens/Order/index.tsx
+++ Ignite/ReactNative/gopizza/src/screens/Order/index.tsx
@@ -1,11 +1,17 @@
 import { Platform } from 'react-native'
 import React from 'react'
-import { Container } from './styles';
+import { Container, Header } from './styles';
+import ButtonBack from '@components/ButtonBack';
 
 const Order = () => {
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
-
+        <Header>
+            <ButtonBack
+                onPress={() => {}}
+                style={{marginBottom: 108}}
+            />
+        </Header>
     </Container>
   )
 }
