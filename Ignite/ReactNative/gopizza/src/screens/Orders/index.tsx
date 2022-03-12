6fafa3390d3447d4f43f66b5f058fce63a515aac
Jhon9191 <joaoantonio15161516@gmail.com>
2022-03-12 10:51:23 -0300

    Orders header

--- Ignite/ReactNative/gopizza/src/screens/Orders/index.tsx
+++ Ignite/ReactNative/gopizza/src/screens/Orders/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import {
   Container,
+  Header,
+  Title,
 } from "./styles";
 
 const Orders = () => {
   return (
     <Container>
+      <Header>
+        <Title>Pedidos feitos</Title>
+      </Header>
     </Container>
   );
 };
--- Ignite/ReactNative/gopizza/src/screens/Orders/styles.ts
+++ Ignite/ReactNative/gopizza/src/screens/Orders/styles.ts
@@ -6,3 +6,18 @@ export const Container = styled.View`
     flex:1;
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
 `;
+
+export const Header = styled(LinearGradient).attrs(({ theme })=>({
+    colors: theme.COLORS.GRADIENT,
+}))`
+    padding: ${getStatusBarHeight() + 33}px 0px 33px;
+`;
+
+export const Title = styled.Text`
+    font-size: 24px;
+    text-align: center;
+    ${({ theme }) => css`
+        font-family: ${theme.FONTS.TITLE};
+        color: ${theme.COLORS.TITLE}
+    `}
+`;
