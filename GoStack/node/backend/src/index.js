75610c1687c888b72997b3678870497f96c167fe
Jhon9191 <joaoantonio15161516@gmail.com>
2022-03-24 21:01:52 -0300

    Http methods

--- GoStack/node/backend/src/index.js
+++ GoStack/node/backend/src/index.js
@@ -2,9 +2,35 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.send({ message: 'Hello world!' })
-})
+app.get('/projects', (request, response) => {
+    return response.json([
+        'PROJETO 1',
+        'PROJETO 2'
+    ]);
+});
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'PROJETO 1',
+        'PROJETO 2',
+        'PROJETO 3'
+    ]);
+});
+
+app.put('/projects:id', (request, response) => {
+    return response.json([
+        'PROJETO 4',
+        'PROJETO 2',
+        'PROJETO 3'
+    ]);
+});
+
+app.delete('/projects:id', (request, response) => {
+    return response.json([
+        'PROJETO 4',
+        'PROJETO 2'
+    ]);
+});
 
 app.listen(3333, () => {
     console.log('🚀Backend started!');
