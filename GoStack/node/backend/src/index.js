f5b065e91dc9d3fe281774ca6f068ec3354ee6fe
Jhon9191 <joaoantonio15161516@gmail.com>
2022-03-25 08:50:01 -0300

    Creating some middleware

--- GoStack/node/backend/src/index.js
+++ GoStack/node/backend/src/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 app.use(express.json());
 
 const projects = [];
+
+function logRequest(request, response, next) {
+    const { method, url } = request;
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+    console.log(logLabel);
+    return next();
+}
+
+function validateProjectId(request, response, next) {
+    const { id } = request.params;
+    if(!(isUuid(id))){
+        return response.status(400).json({error: 'Invalid project ID'})
+    }
+    return next();
+}
+
+app.use(logRequest);
 
 app.get("/projects", (request, response) => {
   const { title } = request.body;
@@ -25,7 +42,7 @@ app.post("/projects", (request, response) => {
   return response.json(project);
 });
 
-app.put("/projects/:id", (request, response) => {
+app.put("/projects/:id", validateProjectId, (request, response) => {
   const { id } = request.params;
   const { title, owner } = request.body;
   const projectIndex = projects.findIndex((project) => project.id === id);
@@ -45,7 +62,7 @@ app.put("/projects/:id", (request, response) => {
   return response.json(project);
 });
 
-app.delete("/projects/:id", (request, response) => {
+app.delete("/projects/:id", validateProjectId, (request, response) => {
   const { id } = request.params;
   const { title, owner } = request.body;
   const projectIndex = projects.findIndex((project) => project.id === id);
@@ -60,5 +77,5 @@ app.delete("/projects/:id", (request, response) => {
 });
 
 app.listen(3333, () => {
-  console.log("🚀Backend started!");
+  console.log("🚀 Backend started!");
 });
