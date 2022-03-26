fc6d11857aadf4543e11464eacb54f1cd18d4d4b
Jhon9191 <joaoantonio15161516@gmail.com>
2022-03-26 00:41:40 -0300

    Using states

--- GoStack/react/frontend-exemplo/src/App.js
+++ GoStack/react/frontend-exemplo/src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 function App() {
-    const projects = [
+
+    const [projects, setProjects] = useState([
         'Desenvolvimento frontEnd',
         'Desenvolvimento mobile'
-    ]
+    ]);
+
+    function handleCreateProject(){
+      setProjects([...projects, `Novo projeto ${Date.now()}`]);
+      console.log(projects);
+    }
+
   return (
     <>
-      <Header title="Helcome 2" />
+      <Header title="Helcome sd2" />
       <ul>
           {projects.map(project => <li key={project}>{project}</li>)}
       </ul>
+      <button type="button" onClick={handleCreateProject}>Adicionar projeto</button>
     </>
   );
 }
