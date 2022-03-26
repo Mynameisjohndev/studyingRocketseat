253988a3db1c65653df92f36a32a908a41870248
Jhon9191 <joaoantonio15161516@gmail.com>
2022-03-25 21:04:36 -0300

    Configuring Webpack

--- GoStack/react/frontend-exemplo/babel.config.js
+++ GoStack/react/frontend-exemplo/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    presetss: [
+    presets: [
         '@babel/preset-env', 
         '@babel/preset-react',
     ]
--- GoStack/react/frontend-exemplo/src/index.js
+++ GoStack/react/frontend-exemplo/src/index.js
@@ -4,4 +4,4 @@ const soma = (a, b) => {
     return a + b;
 }
 
-console.log(soma(1,3))
+console.log(soma(4,3))
