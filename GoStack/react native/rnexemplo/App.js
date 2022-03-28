82960f88009e070ab1f7c57b5d755ee63ec10727
Jhon9191 <joaoantonio15161516@gmail.com>
2022-03-28 09:16:52 -0300

    Difference with ReactJs

---
+++ GoStack/react native/rnexemplo/App.js
@@ -0,0 +1,27 @@
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle='light-content' backgroundColor="#7159c1"/>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello GoStack!</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+});
