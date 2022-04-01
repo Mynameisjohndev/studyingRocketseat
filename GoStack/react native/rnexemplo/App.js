054a482c87917d79eaf644b2c4fde4992832eec9
Jhon9191 <joaoantonio15161516@gmail.com>
2022-04-01 11:08:07 -0300

    Creating New Projects

--- GoStack/react native/rnexemplo/App.js
+++ GoStack/react native/rnexemplo/App.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   StatusBar,
   FlatList,
+  TouchableOpacity,
 } from 'react-native';
 import React, {useEffect, useState} from 'react';
 import api from './src/services/api';
@@ -22,6 +23,14 @@ export default function App() {
     loadProjects();
   }, []);
 
+  async function handleAddProjects() {
+    const response = await api.post('/projects', {
+      title: `Add Project${Date.now()}`,
+      owner: 'João Antônio',
+    });
+    setProjects([...projects, response.data])
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
@@ -31,6 +40,12 @@ export default function App() {
           keyExtractor={project => project.id}
           renderItem={({item}) => <Text>{item.title}</Text>}
         />
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProjects}>
+          <Text style={styles.buttonText}>Adicionar Projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -44,6 +59,18 @@ const styles = StyleSheet.create({
   title: {
     color: '#fff',
     fontSize: 32,
+    fontWeight: 'bold',
+  },
+  button: {
+    margin: 20,
+    height: 50,
+    backgroundColor: '#fff',
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
     fontWeight: 'bold',
   },
 });
--- GoStack/react native/rnexemplo/src/services/api.js
+++ GoStack/react native/rnexemplo/src/services/api.js
@@ -4,6 +4,6 @@ const api = axios.create({
     baseURL: "http://localhost:3333",
 });
 
-// Para testar api local rodar adb rever tcp:3333 tcp:3333
+// Para testar api local rodar adb reverse tcp:3333 tcp:3333
 
 export default api;
