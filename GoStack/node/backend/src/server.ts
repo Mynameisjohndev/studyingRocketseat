fc896a9f4bac75218c1049bc05670018086f75c2
Jhon9191 <joaoantonio15161516@gmail.com>
2022-04-03 00:13:01 -0300

    Configuring initial backend structure

---
+++ GoStack/node/backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', ( request, response ) => {
+    response.json({message: 'Hello world!'});
+});
+
+app.listen(3333, () => {
+    console.log('Server is running 🚀')
+})
