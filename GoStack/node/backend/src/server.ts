5980595fa05807c9bdee1cdbd9391720711f28d0
Jhon9191 <joaoantonio15161516@gmail.com>
2022-04-03 23:21:01 -0300

    Configuring editorConfig

--- GoStack/node/backend/src/server.ts
+++ GoStack/node/backend/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', ( request, response ) => {
-    response.json({message: 'Hello world!'});
+     response.json({message: 'Hello world!'});
 });
 
 app.listen(3333, () => {
