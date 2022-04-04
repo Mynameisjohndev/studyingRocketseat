aa9df4dcbc3cf86f1cf3119fb2c60dfccaaa4f38
Jhon9191 <joaoantonio15161516@gmail.com>
2022-04-04 11:39:40 -0300

    Configurind importing ts archives

--- GoStack/node/backend/src/server.ts
+++ GoStack/node/backend/src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Hello world!' });
-});
-
+app.use(routes);
 app.listen(3333, () => {
   console.log('Server is running 🚀');
 });
