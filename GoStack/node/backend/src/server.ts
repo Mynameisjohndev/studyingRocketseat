7a57aea77d9e6f1b177f0b1afe1b321e59772bd7
Jhon9191 <joaoantonio15161516@gmail.com>
2022-04-03 23:37:19 -0300

    Configuring eslint

--- GoStack/node/backend/src/server.ts
+++ GoStack/node/backend/src/server.ts
@@ -2,10 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', ( request, response ) => {
-     response.json({message: 'Hello world!'});
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello world!' });
 });
 
 app.listen(3333, () => {
-    console.log('Server is running 🚀')
-})
+  console.log('Server is running 🚀');
+});
