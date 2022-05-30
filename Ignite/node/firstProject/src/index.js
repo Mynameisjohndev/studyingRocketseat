4b584d4a71d7d607901fce7b1d47e46934ac6817
Jhon9191 <joaoantonio15161516@gmail.com>
2022-05-30 16:35:41 -0300

    configuring nodemon

--- Ignite/node/firstProject/src/index.js
+++ Ignite/node/firstProject/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response)=>{
-    return response.json('Hello world!');
+    return response.json('Começando novo módulo!');
 })
 
 app.listen(3333);
