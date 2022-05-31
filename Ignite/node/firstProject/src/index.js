10ad4f4f37db540aa9456ae6a4beb60b97ba3e53
Jhon9191 <joaoantonio15161516@gmail.com>
2022-05-30 21:32:13 -0300

    Types of parameters

--- Ignite/node/firstProject/src/index.js
+++ Ignite/node/firstProject/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+
+app.use(express.json());
 /*
 
     REQUISIÇÕES
@@ -13,24 +15,38 @@ const app = express();
     DELETE - DELETAR UMA INFORMAÇÃO NO SERVIDOR
 */
 
+/*
 
+    PARÂMETROS
+
+    ROUTE PRAMS => INDENTIFICAR UM RECURSO (EDITAR/ DELETAR/ BUSCAR);
+
+    QUERY PARAMS => PAGINAÇÃO / FILTRO 
+
+*/
 app.get('/courses', (request, response) => {
+    const query = request.query;
+    console.log(query);
     return response.json(['curso 1, curso 2, curso 3']);
 });
 
 app.post('/courses', (request, response) => {
+    const body = request.body;
+    console.log(body);
     return response.json(['curso 1, curso 2, curso 3, curso 4']);
 });
 
-app.put('/courses:id', (request, response) => {
+app.put('/courses/:id', (request, response) => {
+    const { id } = request.params;
+    console.log(id);
     return response.json(['curso 6, curso 2, curso 3, curso 4']);
 });
 
-app.post('/courses:id', (request, response) => {
+app.post('/courses/:id', (request, response) => {
     return response.json(['curso 6, curso 7, curso 3, curso 4']);
 });
 
-app.delete('/courses:id', (request, response) => {
+app.delete('/courses/:id', (request, response) => {
     return response.json(['curso 6, curso 2, curso 4']);
 });
 
