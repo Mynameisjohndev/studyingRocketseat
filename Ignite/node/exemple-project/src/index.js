82cdc2e709ff812346bdc73c09c9cb187e6db42d
Jhon9191 <joaoantonio15161516@gmail.com>
2022-05-31 09:57:55 -0300

    Validating cpf

--- Ignite/node/exemple-project/src/index.js
+++ Ignite/node/exemple-project/src/index.js
@@ -8,11 +8,17 @@ app.use(express.json());
 
 app.post("/acount", (request, response) => {
     const { cpf, name} = request.body;
-    const id = v4();
+
+    const customerAlreadyExists = customers.some((customer)=>customer.cpf === cpf);
+
+    if(customerAlreadyExists){
+        return response.status(400).json({message: "Customer already exists!"});
+    }
+
     const customer = {
         cpf,
         name,
-        id,
+        id: v4(),
         statement: [],
     }
     customers.push(customer);
