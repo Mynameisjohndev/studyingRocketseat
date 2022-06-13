576fe9df660f6601d7fd3d0b3487c3cdab52a8c0
Myanameisjohndev <joaoantonio15161516@gmail.com>
2022-06-13 14:29:01 -0300

    Creating category

--- Ignite/node/four-api/src/server.ts
+++ Ignite/node/four-api/src/server.ts
@@ -1,14 +1,11 @@
-import express, { request, response } from "express";
+import express from "express";
+
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
+
 app.use(express.json());
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello world!" });
-});
 
-app.post("/courses", (request, response) => {
-    const { name } = request.body;
+app.use(categoriesRoutes);
 
-    return response.json({ name });
-});
 app.listen(3333, () => console.log("Server is running"));
