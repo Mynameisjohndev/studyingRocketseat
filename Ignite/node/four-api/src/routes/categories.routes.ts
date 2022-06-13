08d045fc71b7398da615d969380b4d83ee392421
Myanameisjohndev <joaoantonio15161516@gmail.com>
2022-06-13 16:11:40 -0300

    Validating category create

--- Ignite/node/four-api/src/routes/categories.routes.ts
+++ Ignite/node/four-api/src/routes/categories.routes.ts
@@ -7,6 +7,12 @@ const categoryRepositoryy = new CategoryRepositoryy();
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
+    const categoryAlreadyExists = categoryRepositoryy.findByName(name);
+
+    if (categoryAlreadyExists) {
+        return response.status(400).json({ error: "Category Already Exists" });
+    }
+
     categoryRepositoryy.create({ name, description });
     return response.status(201).send();
 });
