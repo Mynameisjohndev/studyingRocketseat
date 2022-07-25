828cfd7529e645c035a27887309f089e0473fb16
Mynameisjohndev <joaoantonio15161516@gmail.com>
2022-07-25 15:55:49 -0300

    Serving static HTML

--- Ignite/react/01-github-explorer/webpack.config.js
+++ Ignite/react/01-github-explorer/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWEbpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: 'development',
@@ -10,6 +11,11 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  plugins: [
+    new HtmlWEbpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   module: {
     rules: [
       {
