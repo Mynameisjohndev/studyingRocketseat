187954b7d80fb247ba76333590ecb3ff2a3aa350
Mynameisjohndev <joaoantonio15161516@gmail.com>
2022-07-26 15:59:44 -0300

    Configuring miragejs

--- Ignite/react/dtmoney/src/index.tsx
+++ Ignite/react/dtmoney/src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes(){
+      this.namespace = 'api';
+      this.get('/transactions', () => {
+        return [
+          {
+            id: '1',
+            title: 'transaction one',
+            amount: 400,
+            type: 'deposit',
+            category: 'food',
+            createdAt: new Date(),
+          }
+        ]
+      })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
