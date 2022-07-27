27d93dc9e06b6a0b12b05ee4398590a09d7b0e8d
Mynameisjohndev <joaoantonio15161516@gmail.com>
2022-07-26 21:12:41 -0300

    Configuring modal

--- Ignite/react/dtmoney/src/App.tsx
+++ Ignite/react/dtmoney/src/App.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen ] = useState(false);
+
+  function handlepOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handlepCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handlepOpenNewTransactionModal}/>
       <Dashboard/>
       <GlobalStyle/>
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handlepCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
     </>
   );
 }
--- Ignite/react/dtmoney/src/components/Header/index.tsx
+++ Ignite/react/dtmoney/src/components/Header/index.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import Logo from "../../assets/Logo.svg";
 import { Container, Content } from "./styles";
 
-export function Header(){
+interface HeaderProps{
+  onOpenNewTransactionModal: () => void; 
+}
+
+export function Header( { onOpenNewTransactionModal } : HeaderProps){
+
   return(
     <Container>
       <Content>
         <img src={Logo} alt="dtmoney "/>
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal} >
           Nova transação
         </button>
+        
       </Content>
     </Container>
   )
