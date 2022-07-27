dad7088e4db3bd9836f4e145f2e51d63a4da8fe8
Mynameisjohndev <joaoantonio15161516@gmail.com>
2022-07-26 22:39:53 -0300

    Creating button and add functionalities

--- Ignite/react/dtmoney/src/components/NewTransactionModal/index.tsx
+++ Ignite/react/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,12 +1,18 @@
-import { Container } from "./styles";
+import { Container, RadioBox, TransactionTypeContainer} from "./styles";
 import Modal from 'react-modal';
 import Close from '../../assets/Close.svg';
+import incomeImg from '../../assets/Entradas.svg';
+import outcomeImg from '../../assets/Saídas.svg';
+import { useState } from "react";
 interface ModalProps{
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }:ModalProps){
+
+  const [type, setType] = useState('deposit');
+
   return(
     <Modal
       isOpen={isOpen}
@@ -22,10 +28,39 @@ export function NewTransactionModal({ isOpen, onRequestClose }:ModalProps){
       </button>
       <Container>
         <h2>Cadastrar transação</h2>
+
         <input placeholder="Título"/>
+
         <input placeholder="Valor" type="number"/>
+
+        <TransactionTypeContainer>
+
+        <RadioBox 
+          type="button" 
+          onClick={() => setType('deposit')} 
+          isActive={type === 'deposit'}
+          activeColor="green"
+        >
+          <img src={incomeImg} alt="Entradas"/>
+          <span>Entradas</span>
+        </RadioBox>
+
+        <RadioBox 
+          type="button" 
+          onClick={() => setType('withdraw')}
+          isActive={type === 'withdraw'}
+          activeColor="red"
+        >
+          <img src={outcomeImg} alt="Saidas"/>
+          <span>Saídas</span>
+        </RadioBox>
+
+        </TransactionTypeContainer>
+
         <input placeholder="Categoria" />
+
         <button type="submit">Cadastrar</button>
+
       </Container>
     </Modal>
   )
