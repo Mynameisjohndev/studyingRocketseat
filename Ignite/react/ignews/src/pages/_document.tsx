f797cbf077b18a9c5b70f1f95cf4c6dd0b83485f
Mynameisjohndev <joaoantonio15161516@gmail.com>
2022-08-08 13:23:41 -0300

    Dynamic title for each page

--- Ignite/react/ignews/src/pages/_document.tsx
+++ Ignite/react/ignews/src/pages/_document.tsx
@@ -1,13 +1,11 @@
-import NextDocument, { Html,  Head, Main, NextScript } from "next/document"
-
+import NextDocument, { Html,  Head, Main, NextScript } from "../../node_modules/next/document"
 export default class MyDocument extends NextDocument{
   render(){
     return(
       <Html >
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;600&family=Roboto:wght@900&display=swap" rel="stylesheet"/>
-          <title>Ignews</title>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;600;900&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main/>
--- Ignite/react/ignews/src/pages/index.tsx
+++ Ignite/react/ignews/src/pages/index.tsx
@@ -1,8 +1,14 @@
 import styles from '../../styles/home.module.scss'
 
+import Head from '../../node_modules/next/head'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello world</h1>
+    <>
+      <Head>
+        <title>Inicio | Ignews</title>
+      </Head>
+      <h1 className={styles.title}>Hello world</h1>
+    </>
   )
 }
